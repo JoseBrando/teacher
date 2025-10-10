0d6e605d186421e9f2949a26bb48cacb18656223
Brando <jose.brando.c.m@gmail.com>
2025-10-10 10:26:49 -0600

    add adminlte

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// === CSS ===
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'admin-lte/dist/css/adminlte.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+// === JS ===
+import 'jquery'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'admin-lte/dist/js/adminlte.min.js'
 
 const app = createApp(App)
 
